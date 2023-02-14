5d14dfc0dc36d406cc28b3a5d635f583d71c0698
zmj <1312970787@qq.com>
2023-02-15 00:09:40 +0800

    2-15

--- src/main/java/com/zmj/FileOperate.java
+++ src/main/java/com/zmj/FileOperate.java
@@ -3,20 +3,26 @@ package com.zmj;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 
 /**
  * @author zhangmingjian
  * @date 2023/2/14
  */
 public class FileOperate {
+    private static final Logger logger = LoggerFactory.getLogger(FileOperate.class);
     
     /**
      * 读取excel
      */
-    public static void readExcel(InputStream inputStream) {
+    public static void readExcel(InputStream inputStream, String fileName) {
+        
+        BigDecimal[][] data = new BigDecimal[4][15];
         
         try (
                 XSSFWorkbook workbook = new XSSFWorkbook(inputStream)
@@ -24,17 +30,21 @@ public class FileOperate {
             Sheet sheet = workbook.getSheetAt(0);
             // 最后一行行号
             int lastRowNum = sheet.getLastRowNum();
-            System.out.println(lastRowNum);
-            for (int i = 0; i <= lastRowNum; i++) {
+            for (int i = 1; i <= lastRowNum; i++) {
                 Row row = sheet.getRow(i);
-                System.out.println(row.getLastCellNum());
-                for (int j = 0; j < row.getLastCellNum(); j++) {
-                    System.out.print(row.getCell(j) + "\t");
+                for (int j = 2; j < row.getLastCellNum(); j++) {
+                    data[i - 1][j - 2] = BigDecimal.valueOf(row.getCell(j).getNumericCellValue());
                 }
-                System.out.println();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        for (int i = 0; i < data[0].length; i++) {
+            BigDecimal val1 = data[0][i].add(data[1][i]);
+            BigDecimal val2 = data[0][i].subtract(data[2][i]);
+            if (val1.compareTo(data[3][i]) > 0 || val2.compareTo(data[3][i]) < 0) {
+                logger.warn("文件" + fileName + " 第 " + (i + 2) + " 列超差!");
+            }
         }
         
     }
--- src/main/java/com/zmj/Main.java
+++ src/main/java/com/zmj/Main.java
@@ -1,18 +1,43 @@
 package com.zmj;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * @author zhangmingjian
- * @date ${DATE}
+ * @date 2021-2-15
  */
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Hello world!");
-        
-        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\zgw4526\\Desktop\\overproof\\1111.csv");
-        FileOperate.readExcel(fileInputStream);
-        
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    
+    public static void main(String[] args) throws IOException {
+        logger.info("Run...");
+        File source = new File("source");
+        File complete = new File("complete");
+        if (!source.exists()) {
+            source.mkdirs();
+        }
+        if (!complete.exists()) {
+            complete.mkdirs();
+        }
+        File[] files = source.listFiles();
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                String fileName = file.getName();
+                // 读文件计算
+                FileOperate.readExcel(Files.newInputStream(file.toPath()), fileName);
+                // 移动文件
+                String fileNameOrigin = fileName.substring(0, fileName.lastIndexOf("."));
+                FileUtils.moveFile(file, new File("complete/" + fileNameOrigin + "_" + System.currentTimeMillis() + ".csv"));
+            }
+        } else {
+            logger.info("没有目录或文件, 要将文件放到程序根目录下的source目录下, 已读过的文件会移到complete目录下!");
+        }
+        logger.info("End...");
     }
 }
