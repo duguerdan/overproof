07de17007e5c1c69532e779de9cfd54d57c7a222
zhangmingjian <GW.Zhangmingjian@h3c.com>
2023-02-15 13:02:40 +0800

    音频播放

---
+++ src/main/java/com/zmj/Media.java
@@ -0,0 +1,31 @@
+package com.zmj;
+
+import java.io.File;
+import java.net.URI;
+import java.util.Objects;
+
+/**
+ * @author zhangmingjian
+ * @date 2023/2/15
+ */
+public class Media {
+    
+    public static void play() {
+        String path = Objects.requireNonNull(Media.class.getClassLoader().getResource("media/prompt.wav")).getPath();
+        File file = new File(path);
+        URI uri = file.toURI();
+        javafx.scene.media.AudioClip audioClip = new javafx.scene.media.AudioClip(uri.toString());
+        // audioClip.setCycleCount(2);// 播放次数
+        audioClip.play();
+        try {
+            // 延迟，不关闭main用于播放音乐
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static void main(String[] args) {
+        play();
+    }
+}
